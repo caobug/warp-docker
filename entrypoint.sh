6e47fd317cd4de048dfb354fcf1ef327f3a183f6
caobug <7737673+caobug@users.noreply.github.com>
2023-12-09 02:34:13 +0800

    some improvements
    - Changed the Ubuntu to the more stable Debian
    - Supported Zero Trust
    - Fix entrypoint.sh permission issue
    - Added automatic restart example

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,16 +11,29 @@ chmod 600 /dev/net/tun
 # start the daemon
 warp-svc &
 
-# sleep to wait for the daemon to start, default 2 seconds
-sleep "$WARP_SLEEP"
-
 # if /var/lib/cloudflare-warp/reg.json not exists, register the warp client
 if [ ! -f /var/lib/cloudflare-warp/reg.json ]; then
-    warp-cli register && echo "Warp client registered!"
-    # if a license key is provided, register the license
-    if [ -n "$WARP_LICENSE_KEY" ]; then
-        echo "License key found, registering license..."
-        warp-cli set-license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
+    # sleep to wait for the daemon to start, default 2 seconds
+    sleep "$WARP_SLEEP"
+    if [ -n "$WARP_ORGANIZATION" ]; then
+        {
+            echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            echo "<dict>"
+            echo "    <key>organization</key>"
+            echo "    <string>$WARP_ORGANIZATION</string>"
+            echo "    <key>auth_client_id</key>"
+            echo "    <string>$WARP_AUTH_CLIENT_ID</string>"
+            echo "    <key>auth_client_secret</key>"
+            echo "    <string>$WARP_AUTH_CLIENT_SECRET</string>"
+            echo "</dict>"
+        }>/var/lib/cloudflare-warp/mdm.xml
+    else
+        warp-cli register && echo "Warp client registered!"
+        # if a license key is provided, register the license
+        if [ -n "$WARP_LICENSE_KEY" ]; then
+            echo "License key found, registering license..."
+            warp-cli set-license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
+        fi
     fi
     # connect to the warp server
     warp-cli connect
